0f0c359fca51bef4fc5613626c9646223d0955c4
Dr Kevin Buffardi <kbuffardi@csuchico.edu>
2017-11-13 08:25:00 +0000

    Starter for reading from file

--- main.cpp
+++ main.cpp
@@ -1,29 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream> // <-- need for File IO
+
 using namespace std;
 
-//Global constants...
-const int MAX_EXERCISES = 10;
-const int MAX_STUDENTS = 5;
-
 //Function prototypes...
-vector < vector <double> > fillGradebook(int,int);
-int getNum(string,int);
 void display(vector <double> );
+vector <double> averageAssignment( vector <vector <double> >);
 
 //Main program...
 int main()
 {
+  string filename;
   int exercises=0;
   int students=0;
   vector < vector<double> > gradebook;
   vector <double> avgGrade;
-
-  exercises = getNum("exercises",MAX_EXERCISES);
-  students = getNum("students",MAX_STUDENTS);
-  gradebook = fillGradebook(students,exercises);
   
+  cout<<"Enter gradebook file: ";
+  cin>>filename;
+  gradebook = loadGrades( filename );
   avgGrade = averageAssignment( gradebook ); // <-- IMPLEMENT FUNCTION FOR THIS
   
   display(avgGrade);
@@ -31,40 +28,23 @@ int main()
   return 0;
 }
 
-vector < vector <double> > fillGradebook(int students, int exercises)
+vector <double> averageAssignment( vector <vector <double> > all)
 {
-  vector < vector<double> > grades;
-  //make row for each student
-  grades.resize(students);
-
-  for(int student=0; student < students; student++ )
+  vector <double> result;
+  double columnSum;
+  int rows = all.size();
+  int cols = all[0].size();
+  
+  for(int c = 0; c < cols; c++)
   {
-    cout<<"Enter student "<<(student+1)<<"'s "
-        <<exercises<<" grades: ";
-    for(int exercise=0; exercise < exercises; exercise++)
+    columnSum = 0.0;
+    for(int r=0; r < rows; r++)
     {
-      //add a column for each student's grade
-      double grade;
-      cin>>grade;
-      grades[student].push_back(grade);
+      columnSum = columnSum + all[r][c];
     }
+    result.push_back(columnSum/rows);
   }
-  return grades;
-}
-
-int getNum(string name,int maximum)
-{
-  int count = 0;
-  do
-  {
-    cout<<"How many "<<name<<" are there?"<<endl;
-    cin>>count;
-    if( count < 1 || count > maximum )
-    {
-      cout<<"ERROR: Must be between 1-"<<maximum<<".\n";
-    }
-  }while( count < 1 || count > maximum);
-  return count;
+  return result;
 }
 
 void display(vector <double> grades)
