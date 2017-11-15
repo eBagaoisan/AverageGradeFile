00339c9baf6b7c4954c776bca4a597e90b2797c4
ebagaoisan <ebagaoisan1@mail.csuchico.edu>
2017-11-15 10:36:40 -0800

    Fixed loadGrades functions.

--- main.cpp
+++ main.cpp
@@ -8,11 +8,12 @@ using namespace std;
 //Function prototypes...
 void display(vector <double> );
 vector <double> averageAssignment( vector <vector <double> >);
+vector<vector<double> > loadGrades(char *);
 
 //Main program...
 int main()
 {
-  string filename;
+  char *filename;
   int exercises=0;
   int students=0;
   vector < vector<double> > gradebook;
@@ -20,7 +21,7 @@ int main()
   
   cout<<"Enter gradebook file: ";
   cin>>filename;
-  gradebook = loadGrades( filename ); // <-- IMPLEMENT FUNCTION FOR THIS
+  gradebook = loadGrades(filename); // <-- IMPLEMENT FUNCTION FOR THIS
   avgGrade = averageAssignment( gradebook );
   
   display(avgGrade);
@@ -32,11 +33,14 @@ int main()
 //1-dimensional vector of each column's average.
 vector <double> averageAssignment( vector <vector <double> > all)
 {
+    cout<<"run?"<<endl;
+
   vector <double> result;
   double columnSum;
   int rows = all.size();
   int cols = all[0].size();
   
+  cout<<"inserting"<<endl;
   for(int c = 0; c < cols; c++)
   {
     columnSum = 0.0;
@@ -59,4 +63,31 @@ void display(vector <double> grades)
     cout<<grades[i]<<" ";
   }
   cout<<endl;
+}
+
+//implement function for loadgrades()
+vector<vector<double> > loadGrades(char *file){
+    vector<vector<double> > grades;
+    ifstream inputFile(file);
+    double number;
+
+    grades.resize(3);
+
+    if(inputFile.is_open()){
+        cout<< "Opened "<<file<<endl;
+        for (int i = 0; i<3; i++){
+            for (int j = 0; j<4; j++){
+                inputFile>>number;
+                cout<<number<< " ";
+                grades[i].push_back(number);
+            }
+            cout<<endl;
+        }
+        //inputFile>>number;
+        cout<<"First grade is "<<number<<endl;
+    }
+    else{
+        cout<<"Failed to open gradebook file "<<file<<endl;
+    }
+    return grades;
 }
